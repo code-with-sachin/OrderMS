e18ad24a762c38931893f42bcf9c4ccb05bc01b0
Sachin <sachinkaushikofficial@gmail.com>
2025-03-31 15:59:27 +0530

    Initial commit

---
+++ src/main/java/com/sachinsk/OrderMS/OrderMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.sachinsk.OrderMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sachinsk/OrderMS/OrderMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachinsk.OrderMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
